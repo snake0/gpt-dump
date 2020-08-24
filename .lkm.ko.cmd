22e721a105f545fc8574b05913453c05a77f8b79
snake0 <jiaxg1998@sjtu.edu.cn>
2020-08-24 09:47:25 -0400

    add binaries

---
+++ .lkm.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/snake0/lkm/lkm.ko := ld -r -m elf_x86_64 -T ./scripts/module-common.lds --build-id  -o /home/snake0/lkm/lkm.ko /home/snake0/lkm/lkm.o /home/snake0/lkm/lkm.mod.o ;  true
---
+++ .tmp_versions/lkm.mod
@@ -0,0 +1,3 @@
+/home/snake0/lkm/lkm.ko
+/home/snake0/lkm/lkm.o
+
