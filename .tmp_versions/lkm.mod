84ab2cf50361ef477d7768f3b0f403853764a718
snake0 <1260865816@qq.com>
2020-09-08 22:10:59 +0800

    sort

--- .tmp_versions/lkm.mod
+++
@@ -1,3 +0,0 @@
-/home/snake0/lkm/lkm.ko
-/home/snake0/lkm/lkm.o
-
--- lkm.mod.c
+++ lkm.mod.c
@@ -1,11 +1,15 @@
+#include <linux/build-salt.h>
 #include <linux/module.h>
 #include <linux/vermagic.h>
 #include <linux/compiler.h>
 
+BUILD_SALT;
+
 MODULE_INFO(vermagic, VERMAGIC_STRING);
+MODULE_INFO(name, KBUILD_MODNAME);
 
 __visible struct module __this_module
-__attribute__((section(".gnu.linkonce.this_module"))) = {
+__section(.gnu.linkonce.this_module) = {
 	.name = KBUILD_MODNAME,
 	.init = init_module,
 #ifdef CONFIG_MODULE_UNLOAD
@@ -14,10 +18,11 @@ __attribute__((section(".gnu.linkonce.this_module"))) = {
 	.arch = MODULE_ARCH_INIT,
 };
 
-static const char __module_depends[]
-__used
-__attribute__((section(".modinfo"))) =
-"depends=";
+#ifdef CONFIG_RETPOLINE
+MODULE_INFO(retpoline, "Y");
+#endif
+
+MODULE_INFO(depends, "");
 
 
-MODULE_INFO(srcversion, "5075EF520844304F9B49E0B");
+MODULE_INFO(srcversion, "A47B7A1C2F62DC32CFAC57B");
