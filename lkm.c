9e8354179d5951d589a3b12bc3b1d87e648128c9
snake0 <1260865816@qq.com>
2021-04-02 10:41:58 +0800

    update format

--- lkm.c
+++ lkm.c
@@ -92,26 +92,27 @@ MODULE_LICENSE("GPL");
 #define PADDR_PATTERN \
   PTE_PHYADDR_PATTREN VADDR_OFFSET_PATTERN
 
+static inline void pr_sep(void) {
+  // pr_err(" ...........................................................................\n");
+  pr_err("\n");
+}
+
 /* static vals */
 unsigned long vaddr, paddr, pgd_idx, pud_idx, pmd_idx, pte_idx;
-const char *prefixes[] = {"pgd", "pud", "pmd", "pte"};
 const char *PREFIXES[] = {"PGD", "PUD", "PMD", "PTE"};
 
 /* static inline functions */
-static inline void print_ulong_pte(unsigned long address, unsigned long ulong, 
+static inline void pr_pte(unsigned long address, unsigned long pte, 
                                   unsigned long i, int level) {
-  pr_cont(" PhyAddr  =  ");
-
   if (level == 1)
-    pr_cont(PADDR_PATTERN" (From CR3) \n", UL_TO_PADDR(address));
+    pr_cont(" GPA  CR3       =      ");
   else
-    pr_cont(PADDR_PATTERN" (From %s) \n", UL_TO_PADDR(address), prefixes[level - 2]);
-  
-  pr_cont("          +  ");
-  pr_cont(PADDR_PATTERN" * 64 (From %s Idx)\n", UL_TO_PADDR((i)), PREFIXES[level - 1]);
+    pr_cont(" GPA  %s       =      ",  PREFIXES[level - 2]);
+  pr_cont(PTE_PHYADDR_PATTREN, UL_TO_PTE_PHYADDR(address >> PAGE_SHIFT));
+  pr_cont(" +  64 * %3lu\n", i);
 
-  pr_cont(" %3lu: %s " PTE_PATTERN"\n", i, prefixes[level - 1], UL_TO_PTE(ulong));
-  pr_err("-----------------------------------------------------------------------------------------\n");
+  pr_cont(" %3lu: %s " PTE_PATTERN"\n", i, PREFIXES[level - 1], UL_TO_PTE(pte));
+  pr_sep();
 }
 
 static inline void print_ptr_vaddr(volatile unsigned long *ptr) {
@@ -122,33 +123,19 @@ static inline void print_ptr_vaddr(volatile unsigned long *ptr) {
   pud_idx = (vaddr >> 30) & mask;
   pmd_idx = (vaddr >> 21) & mask;
   pte_idx = (vaddr >> 12) & mask;
-  pr_info("GPT PGD index: %lu", pgd_idx);
-  pr_info("GPT PUD index: %lu", pud_idx);
-  pr_info("GPT PMD index: %lu", pmd_idx);
-  pr_info("GPT PTE index: %lu", pte_idx);
+  pr_info(" GPT PGD index: %lu", pgd_idx);
+  pr_info(" GPT PUD index: %lu", pud_idx);
+  pr_info(" GPT PMD index: %lu", pmd_idx);
+  pr_info(" GPT PTE index: %lu", pte_idx);
 
-  pr_info("         %lu       %lu       %lu       %lu", pgd_idx, pud_idx, pmd_idx, pte_idx);
-  pr_info("GVA [PGD Idx] [PUD Idx] [PMD Idx] [PTE Idx] [  Offset  ]");
-  pr_info("GVA "VADDR_PATTERN"\n", UL_TO_VADDR(vaddr));
+  pr_info("          %lu       %lu       %lu       %lu", pgd_idx, pud_idx, pmd_idx, pte_idx);
+  pr_info(" GVA [PGD IDX] [PUD IDX] [PMD IDX] [PTE IDX] [  Offset  ]");
+  pr_info(" GVA "VADDR_PATTERN"\n", UL_TO_VADDR(vaddr));
 }
 
 static inline void print_pa_check(unsigned long vaddr) {
-  // unsigned long pfn, offset;
-
   paddr = __pa(vaddr);
-  // pfn = paddr >> PAGE_SHIFT;
-  // offset = paddr & ((1 << PAGE_SHIFT) - 1);
-
-  // pr_info("... ... ... ...  73: pte 100000000000 0000000000000000000100111011 000001100011");
-  // pr_info("  35: pte 100000000000 0000000000000000000100110101010000100011 000001100011");
-  pr_info("        __pa(vaddr) =  " PADDR_PATTERN "\n", UL_TO_PADDR(paddr));
-  // pr_err("-----------------------------------------------------------------------------------------");
-  // pr_info("VADDR 100101100 010101111 111011110 010010011 ");
-
-  // pr_info("VADDR [  PGD  ] [  PUD  ] [  PMD  ] [  PTE  ] [  Offset  ]");
-  // pr_info("VADDR "VADDR_PATTERN"\n", UL_TO_VADDR(vaddr));
-  // pr_info("                     Offset by __pa()         \n" 
-  //   VADDR_OFFSET_PATTERN, UL_TO_PTE_OFFSET(offset));
+  pr_info(" GPA            =      " PADDR_PATTERN "\n", UL_TO_PADDR(paddr));
 }
 
 /* page table walker functions */
@@ -179,15 +166,8 @@ void cleanup_module(void) {}
 void dump_pgd(pgd_t *pgtable, int level) {
   unsigned long i;
   pgd_t pgd;
-  pr_err("-----------------------------------------------------------------------------------------\n");
-  
-  pr_cont("CR3:         ");
-  pr_cont(PADDR_PATTERN "\n", UL_TO_PADDR(__pa(pgtable)));
+  pr_sep();
 
-  pr_err("Page Table Printing Format:\n");
-  pr_err(" Idx: Lvl [Rsvd./Ign.] [     Physical Frame Number, 40 Bits   ] [   Flags  ]");
-  pr_err(" Idx: Lvl [ 12 Bits  ] [ Physical Address is 52-bit Wide for Intel Core i7 ]\n");
-  pr_err("-----------------------------------------------------------------------------------------\n");
 
   for (i = 0; i < PTRS_PER_PGD; i++) {
     pgd = pgtable[i];
@@ -195,7 +175,7 @@ void dump_pgd(pgd_t *pgtable, int level) {
     if (pgd_val(pgd)) {
       if (pgd_present(pgd) && !pgd_large(pgd)) {
         if (i == pgd_idx) {
-          print_ulong_pte(__pa(pgtable), pgd_val(pgd), i, level);
+          pr_pte(__pa(pgtable), pgd_val(pgd), i, level);
 
           dump_pud((pud_t *)pgd_page_vaddr(pgd), level + 1);
         }
@@ -214,7 +194,7 @@ void dump_pud(pud_t *pgtable, int level) {
     if (pud_val(pud)) {
       if (pud_present(pud) && !pud_large(pud)) {
         if (i == pud_idx) {
-          print_ulong_pte(__pa(pgtable), pud_val(pud), i, level);
+          pr_pte(__pa(pgtable), pud_val(pud), i, level);
 
           dump_pmd((pmd_t *)pud_page_vaddr(pud), level + 1);
         }
@@ -233,7 +213,7 @@ void dump_pmd(pmd_t *pgtable, int level) {
     if (pmd_val(pmd)) {
       if (pmd_present(pmd) && !pmd_large(pmd)) {
         if (i == pmd_idx) {
-          print_ulong_pte(__pa(pgtable), pmd_val(pmd), i, level);
+          pr_pte(__pa(pgtable), pmd_val(pmd), i, level);
 
           dump_pte((pte_t *)pmd_page_vaddr(pmd), level + 1);
         }
@@ -252,7 +232,7 @@ void dump_pte(pte_t *pgtable, int level) {
     if (pte_val(pte)) {
       if (pte_present(pte)) {
         if (i == pte_idx)
-          print_ulong_pte(__pa(pgtable), pte_val(pte), i, level);
+          pr_pte(__pa(pgtable), pte_val(pte), i, level);
       }
     }
   }
